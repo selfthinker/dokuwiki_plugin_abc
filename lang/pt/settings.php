58f8e19f24ca2b5814664b8a7d5a3d92212e46fb
João Lopes <joao@ribeirolopes.org>
2013-09-14 22:15:14 +0200

    translation update

---
+++ lang/pt/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author João Lopes <joao@ribeirolopes.org>
+ */
+$lang['displayType']           = 'Como deve ser o resultado apresentado';
+$lang['displayType_o_0']       = 'Imagem apenas';
+$lang['displayErrorlog']       = 'Os logs de erro devem ser apresentados? (apenas uma vez quando gravado)';
+$lang['params4img']            = 'Parametros para abcm2ps para gerar imagens (opcional, para especialistas apenas)';
+$lang['params4ps']             = 'Parametros para abcm2ps para gerar os ficheiros ps/pdf (opcional, para especialistas apenas)';

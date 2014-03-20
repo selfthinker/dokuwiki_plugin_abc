32b96515cb405cf024bdf703cafe193725dcb297
Rene <wllywlnt@yahoo.com>
2014-03-20 09:00:18 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['abcok']                 = 'Mag ABC worden ingesloten?';
+$lang['displayType']           = 'Hoe moet de output worden weergegeven';
+$lang['displayType_o_0']       = 'alleen afbeelding';
+$lang['displayType_o_1']       = 'afbeelding gelinkt aan midi';
+$lang['displayType_o_2']       = 'afbeelding met lijst van abc, midi, ps/pdf';
+$lang['displaySource']         = 'Moet de abc bron worden getoond?';
+$lang['displayErrorlog']       = 'Moet de foutlog worden getoond? (alleen bij opslaan)';
+$lang['abc2ps']                = 'Waar bevindt zich abcm2ps ( of abc2ps of jcabc2ps of jaabc2ps of yaps)';
+$lang['abc2midi']              = 'Waar bevindt zich abc2midi (optioneel als de output is "alleen bestand")';
+$lang['ps2pdf']                = 'Waar bevindt zich ps2pdf (optioneel, vereist indien de output is "afbeelding met lijst van abc, midi, ps/pdf" als het pdf bestand in plaats van een ps bestand dient te worden gegenereerd)';
+$lang['abc2abc']               = 'Waar bevindt zich abc2abc (laat leeg indien transport niet is toegestaan)';
+$lang['fmt']                   = 'Waar bevindt zich een abcm2ps bestand (i.e. " foo.fmt") om HET PS/PDF bestand op te maken ( optioneel)';
+$lang['params4img']            = 'Parameters voor abcm2ps bij het genereren van de afbeelding (optioneel, alleen voor experts!)';
+$lang['params4ps']             = 'Parameters voor abcm2ps bij het genereren van het ps/pdf bestand (optioneel, alleen voor experts!)';
+$lang['mediaNS']               = 'Namespace voor ABC bestanden (laat leeg indien root)';

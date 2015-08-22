587f3415e0523c4c0c480cdd0a8856c956dd1536
Padhie <develop@padhie.de>
2015-08-22 11:25:04 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Padhie <develop@padhie.de>
+ */
+$lang['abcok']                 = 'Wurde ABC eingebunden?';
+$lang['displayType']           = 'Wie soll die Ausgabe angezeigt werden';
+$lang['displayType_o_0']       = 'Nur Bilder';
+$lang['mediaNS']               = 'Namespace für ABC-Dateien (leer lassen für Hauptverzeichnis)';

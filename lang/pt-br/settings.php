0a3e4987c61b64b3c35a8ea4afb1ce9dfdb5a642
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 14:45:04 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['abcok']                 = 'O ABC pode ser incorporado?';
+$lang['displayType']           = 'Como exibir a saída';
+$lang['displayType_o_0']       = 'Apenas imagem';
+$lang['displayType_o_1']       = 'imagem ligada ao midi';
+$lang['displayType_o_2']       = 'imagem com lista de abc, midi, ps / pdf';
+$lang['displaySource']         = 'A fonte abc também será mostrada?';
+$lang['displayErrorlog']       = 'Os logs de erro serão exibidos? (apenas uma vez quando salvo)';
+$lang['abc2ps']                = 'Onde encontrar abcm2ps (ou abc2ps ou jcabc2ps ou jaabc2ps ou yaps)';
+$lang['abc2midi']              = 'Onde encontrar abc2midi (opcional se a saída for "somente imagem")';
+$lang['ps2pdf']                = 'Onde encontrar ps2pdf (opcional, necessário apenas se a saída for "imagem com lista de abc, midi, ps / pdf" e se um arquivo pdf em vez de um arquivo ps for gerado)';
+$lang['abc2abc']               = 'Onde encontrar abc2abc (deixe em branco para não permitir a transposição)';
+$lang['fmt']                   = 'Onde encontrar um arquivo de formato abcm2ps (por exemplo, "foo.fmt") para formatar o arquivo PS / PDF (opcional)';
+$lang['params4img']            = 'Parâmetros para abcm2ps ao gerar a imagem (opcional, apenas para especialistas!)';
+$lang['params4ps']             = 'Parâmetros para abcm2ps ao geraro arquivo ps/pdf (opcional, apenas para especialistas!)';
+$lang['mediaNS']               = 'Namespace para arquivos ABC (deixe em branco para raiz)';

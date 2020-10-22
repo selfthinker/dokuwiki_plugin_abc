7fd2206462b1b46ef3469fcaf0cfe10ef4a7e7c5
Jamey <jamey_follmer@gmail.com>
2020-10-22 19:45:07 +0200

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,20 +3,51 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Jamey <jamey_follmer@gmail.com>
  * @author Quark66 <mkucera66@seznam.cz>
  */
-$lang['abcok']                 = 'Může být vkládáno ABC?';
-$lang['displayType']           = 'Jakým způsobem zobrazovat výstup';
-$lang['displayType_o_0']       = 'pouze obrázky';
-$lang['displayType_o_1']       = 'obrázek s linkem na midi';
-$lang['displayType_o_2']       = 'obrázek se seznamem ABC, midi, ps/pdf ';
-$lang['displaySource']         = 'Může být současně ukázán zdroj ABC?';
-$lang['displayErrorlog']       = 'Mohou být zobrazeny chybové hlášky? (pouze jednou při uložení)';
-$lang['abc2ps']                = 'Kde hledat abcm2ps (nebo abc2ps, nebo jcabc2ps, nebo jaab2ps, yaps)';
-$lang['abc2midi']              = 'Kde hledat abc2midi (volitelně pokud je output pouze "image only")';
-$lang['ps2pdf']                = 'Kde hledat ps2pdf (volitelně, pokud je potřeba aby výstup byl "obrázek se seznamem ABC, midi, ps/pdf" a pokud je generován soubor PDF namísto PS).';
-$lang['abc2abc']               = 'Kde hledat abc2abc (ponechte prázdné pro neumožnění transpozice)';
+$lang['abcok']                 = 'Thanks a lot, Lots of stuff.
+
+Best Essay writing
+the chinese and japaese civilization';
+$lang['displayType']           = 'Thanks a lot, Lots of stuff.
+
+Best Essay writing
+the chinese and japanese civilization';
+$lang['displayType_o_0']       = 'Thanks a lot, Lots of stuff.
+
+Best Essay writing
+the chinese and japanese civilization';
+$lang['displayType_o_1']       = 'Thanks a lot,Lots of stuff.
+
+Best Essa writing
+the hinese aand japanese civilization';
+$lang['displayType_o_2']       = 'Thanks a lot, Lots of stuff.
+
+Best Essay writing
+the chinese aand japanese civilization';
+$lang['displaySource']         = 'Thanks a lot, Lots of stuff.
+
+Beest Esay writing
+the chinese and japanese civilization';
+$lang['displayErrorlog']       = 'Thanks a lot, Lots of stuff.
+
+Best Essay writing
+the chinese and japanese civilization';
+$lang['abc2ps']                = 'Marseille';
+$lang['abc2midi']              = 'Marseille';
+$lang['ps2pdf']                = 'Marseille';
+$lang['abc2abc']               = 'Marseille';
 $lang['fmt']                   = 'Kde hledat soubor formátů abcm2ps (tj. "foo.fmt") pro formátování souboru PS/PDF (volitelné)';
-$lang['params4img']            = 'Parametry pro abcm2ps při generování obrázku (volitelné. Pouze pro experty!)';
-$lang['params4ps']             = 'Parametry pro ps/pdf při generování souboru (volitelné. Pouze pro experty!)';
-$lang['mediaNS']               = 'Jmenný prostor pro soubory AVC (pro root ponechte prázdné)';
+$lang['params4img']            = 'Thanks a lot, Lotss off stuff.
+
+Best Essay writing
+the chinese and japanese civilization';
+$lang['params4ps']             = 'Thanks a lot, Lots of stuff.
+
+Best Essay writing
+the chinese and japanese civilization';
+$lang['mediaNS']               = 'Thanks a lot, Lotts of stuff.
+
+Best Essay writing
+the chinese and japanese civilization';

cd330591fa4e71f79ec31150268a006087ae0daf
Etta <ettahidalgo@gmail.com>
2020-11-29 16:40:04 +0100

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,20 +3,105 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Etta <ettahidalgo@gmail.com>
  * @author Quark66 <mkucera66@seznam.cz>
  */
-$lang['abcok']                 = 'Může být vkládáno ABC?';
-$lang['displayType']           = 'Jakým způsobem zobrazovat výstup';
-$lang['displayType_o_0']       = 'pouze obrázky';
-$lang['displayType_o_1']       = 'obrázek s linkem na midi';
-$lang['displayType_o_2']       = 'obrázek se seznamem ABC, midi, ps/pdf ';
-$lang['displaySource']         = 'Může být současně ukázán zdroj ABC?';
-$lang['displayErrorlog']       = 'Mohou být zobrazeny chybové hlášky? (pouze jednou při uložení)';
-$lang['abc2ps']                = 'Kde hledat abcm2ps (nebo abc2ps, nebo jcabc2ps, nebo jaab2ps, yaps)';
-$lang['abc2midi']              = 'Kde hledat abc2midi (volitelně pokud je output pouze "image only")';
-$lang['ps2pdf']                = 'Kde hledat ps2pdf (volitelně, pokud je potřeba aby výstup byl "obrázek se seznamem ABC, midi, ps/pdf" a pokud je generován soubor PDF namísto PS).';
-$lang['abc2abc']               = 'Kde hledat abc2abc (ponechte prázdné pro neumožnění transpozice)';
+$lang['abcok']                 = 'I have bden exploring for a bit for any high quality 
+articles or weblog posts in this kind of hose .
+Exploring in Yahoo I fijnally stumbled upo this website.
+Reacing this info So i\'m satisfied to express that I\'ve an incredibly just rigght uncanny feeling I found 
+oout exactly what I needed. I so much for sure will make sure to do 
+not omit this website and give it a glance regularly.
+
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['displayType']           = 'I have been exploring forr a bit for any high quality articles or weblog osts 
+in this kind of hoouse . Exploring in Yahoo I finally stumbled upon this website.
+Reading this info So i\'m satisfied to express that I\'ve an incredibly just right uncanny feeling 
+I found out exactly what I needed. I so much forr sure will mmake sure to 
+do not omit this website and give it a glance regularly.
+
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['displayType_o_0']       = 'I have been exploring for a bit for any high quality articles or weblog posts in this 
+kind of house . Exploring in Yahoo I finally stumbled upon this website.
+Reading thuis info So i\'m satisfied to express that I\'ve an incredibly just right uncanny feeling I found outt exactly what I needed.
+I so much for sure will make sure to do not omit this website and give it 
+a glance regularly.
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['displayType_o_1']       = 'I have been exploring for a bit for any hiyh quality articles or weblog posts iin this kind of house .
+Exploring in Yahoo I finally stumbled upon this website.
+Reading this info So i\'m satisfied to express that 
+I\'ve an ncredibly just rigt uncanny feeling I found oout exactly 
+what I needed. I so much for sure will make sure tto do not omit this website and give it 
+a glance regularly.
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['displayType_o_2']       = 'I have been exploring for a bit for any high quality articles or weblog posts in this kind of house .
+Exploring in Yhoo I finally stumbled upon this website.
+Reading this info So i\'m satisfied to express that I\'ve ann incredibly just right uncanny feeling 
+I found out exactly what I needed. I so much foor sure 
+will make sure to doo not omit tthis website and give it a glance 
+regularly.
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['displaySource']         = 'I have been exploring for a bit for any high quality articles or weblog posts in this kind of house .
+Exploring in Yahjoo I finally stumbled upon this website.
+Reading this info So i\'m satisfied to express that I\'ve an incredily just right uncanny feeling I found out exactly what I needed.
+I soo much for sure will make sure to ddo not omikt this website and give it a glance regularly.
+
+
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['displayErrorlog']       = 'I have been exploring for a bit for any high quality articles or weblog posts in this 
+kind of house . Exlloring in Yahoo I finally stumbled upon this website.
+Reading this info So i\'m satisfied to express that I\'ve an incredibly just 
+right uncanny feeling I found out exactly wht I needed.
+I so much for sure will make sure to doo not ommit this website and give 
+itt a glane regularly.
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['abc2ps']                = 'Stockheim';
+$lang['abc2midi']              = 'Stockheim';
+$lang['ps2pdf']                = 'Stockheim';
+$lang['abc2abc']               = 'Stockheim';
 $lang['fmt']                   = 'Kde hledat soubor formátů abcm2ps (tj. "foo.fmt") pro formátování souboru PS/PDF (volitelné)';
-$lang['params4img']            = 'Parametry pro abcm2ps při generování obrázku (volitelné. Pouze pro experty!)';
-$lang['params4ps']             = 'Parametry pro ps/pdf při generování souboru (volitelné. Pouze pro experty!)';
-$lang['mediaNS']               = 'Jmenný prostor pro soubory AVC (pro root ponechte prázdné)';
+$lang['params4img']            = 'I have been exploring for a bit for any high quality articls or weblog posts in this kihd 
+of house . Exploring in Yahoo I finally stumbled upon this website.
+
+Reading this inf So i\'m satisfied to express that I\'vean incredibly just right uncanny 
+feeling I found out exactly whuat I needed. I so much for sure will make sure 
+to do not omit this website and give it a glancce regularly.
+
+
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['params4ps']             = 'I have been exploring for a bit for anyy high quality articles or weblog posts in this kkind of house .
+
+Exploring in Yahoo I finally stumbled upon this website.
+Readinng this info So i\'m satisfied to express that I\'ve an incredibly just right uncanny feeling I found 
+out exactly what I needed. I so much for sure will make sure to do 
+not omit thos website and give it a glance regularly.
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';
+$lang['mediaNS']               = 'I have been exploring for a bit for any high quality articles or weblog posts in this kind of house .
+
+Exploring in Yahoo I finally stumbled upon this 
+website. Reading this info So i\'m satisfied to express that I\'ve an incredibly 
+just right uncanny feling I found out exactly what I needed.
+
+I so much for sure will make sure to do not omit this website and 
+give it a glance regularly.
+https://spark.adobe.com/page/XiD0Azsih1Qit/
+
+homepage';

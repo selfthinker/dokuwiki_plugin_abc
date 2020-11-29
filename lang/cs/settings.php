a21e6f56f844966b1c70ee65a8400c67f8c5a1db
Uwe <uwe.wilkins@inbox.com>
2020-11-30 00:45:04 +0100

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,20 +3,51 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Uwe <uwe.wilkins@inbox.com>
  * @author Quark66 <mkucera66@seznam.cz>
  */
-$lang['abcok']                 = 'Může být vkládáno ABC?';
-$lang['displayType']           = 'Jakým způsobem zobrazovat výstup';
-$lang['displayType_o_0']       = 'pouze obrázky';
-$lang['displayType_o_1']       = 'obrázek s linkem na midi';
-$lang['displayType_o_2']       = 'obrázek se seznamem ABC, midi, ps/pdf ';
-$lang['displaySource']         = 'Může být současně ukázán zdroj ABC?';
-$lang['displayErrorlog']       = 'Mohou být zobrazeny chybové hlášky? (pouze jednou při uložení)';
-$lang['abc2ps']                = 'Kde hledat abcm2ps (nebo abc2ps, nebo jcabc2ps, nebo jaab2ps, yaps)';
-$lang['abc2midi']              = 'Kde hledat abc2midi (volitelně pokud je output pouze "image only")';
-$lang['ps2pdf']                = 'Kde hledat ps2pdf (volitelně, pokud je potřeba aby výstup byl "obrázek se seznamem ABC, midi, ps/pdf" a pokud je generován soubor PDF namísto PS).';
-$lang['abc2abc']               = 'Kde hledat abc2abc (ponechte prázdné pro neumožnění transpozice)';
+$lang['abcok']                 = 'Appreciate it, Ample tips.
+
+Best Essay writing
+buy essay online';
+$lang['displayType']           = 'Appreciate it, Ample tips.
+
+Best Essay writing
+buy essay online';
+$lang['displayType_o_0']       = 'Appreciate it, Ample tips.
+
+Best Essay writing
+buy essay online';
+$lang['displayType_o_1']       = 'Appreciate it, Ample tips.
+
+Best Essay writing
+buy essay online';
+$lang['displayType_o_2']       = 'Appreciate it, Ample tips.
+
+Best Essay writing
+buy essay online';
+$lang['displaySource']         = 'Appreciate it, Ample tips.
+
+Best Essay writing
+buy essay online';
+$lang['displayErrorlog']       = 'Appreciate it, Ample tips.
+
+Best Essay writing
+buy essay online';
+$lang['abc2ps']                = 'Karlshamn';
+$lang['abc2midi']              = 'Karlshamn';
+$lang['ps2pdf']                = 'Karlshamn';
+$lang['abc2abc']               = 'Karlshamn';
 $lang['fmt']                   = 'Kde hledat soubor formátů abcm2ps (tj. "foo.fmt") pro formátování souboru PS/PDF (volitelné)';
-$lang['params4img']            = 'Parametry pro abcm2ps při generování obrázku (volitelné. Pouze pro experty!)';
-$lang['params4ps']             = 'Parametry pro ps/pdf při generování souboru (volitelné. Pouze pro experty!)';
-$lang['mediaNS']               = 'Jmenný prostor pro soubory AVC (pro root ponechte prázdné)';
+$lang['params4img']            = 'Appreciate it, Ample tips.
+
+Besst Essay writing
+buy essayy online';
+$lang['params4ps']             = 'Appreciate it, Amplke tips.
+
+Best Essay writing
+buy essay online';
+$lang['mediaNS']               = 'Appeciate it, Ample tips.
+
+Bestt Essay writing
+buy essay online';

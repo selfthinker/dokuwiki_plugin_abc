13014505e93c9934ddb5c14c259c2747b7c53dce
Sima <simadement@gmail.com>
2020-12-28 07:05:29 +0100

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,20 +3,51 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Sima <simadement@gmail.com>
  * @author Quark66 <mkucera66@seznam.cz>
  */
-$lang['abcok']                 = 'Může být vkládáno ABC?';
-$lang['displayType']           = 'Jakým způsobem zobrazovat výstup';
-$lang['displayType_o_0']       = 'pouze obrázky';
-$lang['displayType_o_1']       = 'obrázek s linkem na midi';
-$lang['displayType_o_2']       = 'obrázek se seznamem ABC, midi, ps/pdf ';
-$lang['displaySource']         = 'Může být současně ukázán zdroj ABC?';
-$lang['displayErrorlog']       = 'Mohou být zobrazeny chybové hlášky? (pouze jednou při uložení)';
-$lang['abc2ps']                = 'Kde hledat abcm2ps (nebo abc2ps, nebo jcabc2ps, nebo jaab2ps, yaps)';
-$lang['abc2midi']              = 'Kde hledat abc2midi (volitelně pokud je output pouze "image only")';
-$lang['ps2pdf']                = 'Kde hledat ps2pdf (volitelně, pokud je potřeba aby výstup byl "obrázek se seznamem ABC, midi, ps/pdf" a pokud je generován soubor PDF namísto PS).';
-$lang['abc2abc']               = 'Kde hledat abc2abc (ponechte prázdné pro neumožnění transpozice)';
+$lang['abcok']                 = 'Thanks a lot! An abundance of facts.
+
+Best Essay writing
+essay writing';
+$lang['displayType']           = 'Thanks a lot! An abundance of facts.
+
+Best Essay writing
+essay writing';
+$lang['displayType_o_0']       = 'Thanks a lot! An abundance oof facts.
+
+Best Essay writing
+essay writing';
+$lang['displayType_o_1']       = 'Thanks a lot! An abundance of facts.
+
+Beest Essay writing
+essay writing';
+$lang['displayType_o_2']       = 'Thankis a lot! An abundance of facts.
+
+Best Essay writing
+essay writing';
+$lang['displaySource']         = 'Thanks a lot! An abundance of facts.
+
+Best Essazy writing
+essay writing';
+$lang['displayErrorlog']       = 'Thanks a lot! An abundance of facts.
+
+Best Essay writing
+essay writing';
+$lang['abc2ps']                = 'Villarvolard';
+$lang['abc2midi']              = 'Villarvolard';
+$lang['ps2pdf']                = 'Villarvolard';
+$lang['abc2abc']               = 'Villarvolard';
 $lang['fmt']                   = 'Kde hledat soubor formátů abcm2ps (tj. "foo.fmt") pro formátování souboru PS/PDF (volitelné)';
-$lang['params4img']            = 'Parametry pro abcm2ps při generování obrázku (volitelné. Pouze pro experty!)';
-$lang['params4ps']             = 'Parametry pro ps/pdf při generování souboru (volitelné. Pouze pro experty!)';
-$lang['mediaNS']               = 'Jmenný prostor pro soubory AVC (pro root ponechte prázdné)';
+$lang['params4img']            = 'Thanks a lot! An abundance of facts.
+
+Best Essay writing
+essay writing';
+$lang['params4ps']             = 'Thanks a lot! An abundance of facts.
+
+Beest Essay writing
+essay writing';
+$lang['mediaNS']               = 'Thanks a lot! An abundance of facts.
+
+Best Essay writing
+essay writing';

984a9f9d5a35dd727b8f68f16e6cf8c2e8e0108d
Carin <carinherrmann@t-online.de>
2020-12-31 15:50:08 +0100

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,20 +3,41 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Carin <carinherrmann@t-online.de>
  * @author Quark66 <mkucera66@seznam.cz>
  */
-$lang['abcok']                 = 'Může být vkládáno ABC?';
-$lang['displayType']           = 'Jakým způsobem zobrazovat výstup';
-$lang['displayType_o_0']       = 'pouze obrázky';
-$lang['displayType_o_1']       = 'obrázek s linkem na midi';
-$lang['displayType_o_2']       = 'obrázek se seznamem ABC, midi, ps/pdf ';
-$lang['displaySource']         = 'Může být současně ukázán zdroj ABC?';
-$lang['displayErrorlog']       = 'Mohou být zobrazeny chybové hlášky? (pouze jednou při uložení)';
-$lang['abc2ps']                = 'Kde hledat abcm2ps (nebo abc2ps, nebo jcabc2ps, nebo jaab2ps, yaps)';
-$lang['abc2midi']              = 'Kde hledat abc2midi (volitelně pokud je output pouze "image only")';
-$lang['ps2pdf']                = 'Kde hledat ps2pdf (volitelně, pokud je potřeba aby výstup byl "obrázek se seznamem ABC, midi, ps/pdf" a pokud je generován soubor PDF namísto PS).';
-$lang['abc2abc']               = 'Kde hledat abc2abc (ponechte prázdné pro neumožnění transpozice)';
+$lang['abcok']                 = 'Nicely put. With thanks.
+Best Essay writing
+paper writing service';
+$lang['displayType']           = 'Nicely put. With thanks.
+Best Essay writing
+paper writing service';
+$lang['displayType_o_0']       = 'Nicely put. With thanks.
+Best Essay writing
+paper writing service';
+$lang['displayType_o_1']       = 'Nicely put. With thanks.
+Best Essay writing
+paper writing service';
+$lang['displayType_o_2']       = 'Nicely put. With thanks.
+Best Essay writing
+paper writing service';
+$lang['displaySource']         = 'Nicely put. With thanks.
+Best Essay writing
+paper writing service';
+$lang['displayErrorlog']       = 'Nicely put. With thanks.
+Best Essaay writing
+paper writing service';
+$lang['abc2ps']                = 'Cutts';
+$lang['abc2midi']              = 'Cutts';
+$lang['ps2pdf']                = 'Cutts';
+$lang['abc2abc']               = 'Cutts';
 $lang['fmt']                   = 'Kde hledat soubor formátů abcm2ps (tj. "foo.fmt") pro formátování souboru PS/PDF (volitelné)';
-$lang['params4img']            = 'Parametry pro abcm2ps při generování obrázku (volitelné. Pouze pro experty!)';
-$lang['params4ps']             = 'Parametry pro ps/pdf při generování souboru (volitelné. Pouze pro experty!)';
-$lang['mediaNS']               = 'Jmenný prostor pro soubory AVC (pro root ponechte prázdné)';
+$lang['params4img']            = 'Nicely put. With thanks.
+Best Essay writing
+paper writing service';
+$lang['params4ps']             = 'Nicely put. With thanks.
+Best Esssay writing
+paper writing service';
+$lang['mediaNS']               = 'Nicely put. Withh thanks.
+Best Essay writing
+paper writinjg service';

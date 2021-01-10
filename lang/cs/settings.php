6db7777ae39930250a298f8ca82ee3bbf442e0d7
Jade <jadebarnhart@googlemail.com>
2021-01-10 23:55:30 +0100

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -3,20 +3,41 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Jade <jadebarnhart@googlemail.com>
  * @author Quark66 <mkucera66@seznam.cz>
  */
-$lang['abcok']                 = 'Může být vkládáno ABC?';
-$lang['displayType']           = 'Jakým způsobem zobrazovat výstup';
-$lang['displayType_o_0']       = 'pouze obrázky';
-$lang['displayType_o_1']       = 'obrázek s linkem na midi';
-$lang['displayType_o_2']       = 'obrázek se seznamem ABC, midi, ps/pdf ';
-$lang['displaySource']         = 'Může být současně ukázán zdroj ABC?';
-$lang['displayErrorlog']       = 'Mohou být zobrazeny chybové hlášky? (pouze jednou při uložení)';
-$lang['abc2ps']                = 'Kde hledat abcm2ps (nebo abc2ps, nebo jcabc2ps, nebo jaab2ps, yaps)';
-$lang['abc2midi']              = 'Kde hledat abc2midi (volitelně pokud je output pouze "image only")';
-$lang['ps2pdf']                = 'Kde hledat ps2pdf (volitelně, pokud je potřeba aby výstup byl "obrázek se seznamem ABC, midi, ps/pdf" a pokud je generován soubor PDF namísto PS).';
-$lang['abc2abc']               = 'Kde hledat abc2abc (ponechte prázdné pro neumožnění transpozice)';
+$lang['abcok']                 = 'With thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['displayType']           = 'With thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['displayType_o_0']       = 'With thanks, Awesome stuff!
+Best Esssay writing
+buy esays online';
+$lang['displayType_o_1']       = 'Wiith thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['displayType_o_2']       = 'With thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['displaySource']         = 'Wiith thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['displayErrorlog']       = 'With thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['abc2ps']                = 'Figline Valdarno';
+$lang['abc2midi']              = 'Figline Valdarno';
+$lang['ps2pdf']                = 'Figline Valdarno';
+$lang['abc2abc']               = 'Figline Valdarno';
 $lang['fmt']                   = 'Kde hledat soubor formátů abcm2ps (tj. "foo.fmt") pro formátování souboru PS/PDF (volitelné)';
-$lang['params4img']            = 'Parametry pro abcm2ps při generování obrázku (volitelné. Pouze pro experty!)';
-$lang['params4ps']             = 'Parametry pro ps/pdf při generování souboru (volitelné. Pouze pro experty!)';
-$lang['mediaNS']               = 'Jmenný prostor pro soubory AVC (pro root ponechte prázdné)';
+$lang['params4img']            = 'With thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['params4ps']             = 'With thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
+$lang['mediaNS']               = 'With thanks, Awesome stuff!
+Best Essay writing
+buy essays online';
